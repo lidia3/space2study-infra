b69d601df36a08154eb016dfcd5940ac5b6ed76f
Lidia Reshnivetska <lidiareshnivetska@Lidias-MacBook-Pro.local>
2026-06-28 18:19:28 +0200

    adding public ips for work

--- terraform/inventory.tf
+++ terraform/inventory.tf
@@ -19,6 +19,15 @@ ${aws_instance.database.public_ip}
 ansible_user=ubuntu
 ansible_ssh_private_key_file=/Users/lidiareshnivetska/.ssh/aws-space2study
 ansible_ssh_common_args='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
+
+# Private IPs (EC2 ↔ EC2 communication)
+mongodb_host=${aws_instance.database.private_ip}
+backend_private_ip=${aws_instance.backend.private_ip}
+frontend_private_ip=${aws_instance.frontend.private_ip}
+
+# Public IPs (Browser ↔ Application)
+backend_public_ip=${aws_instance.backend.public_ip}
+frontend_public_ip=${aws_instance.frontend.public_ip}
 EOF
 
 }
