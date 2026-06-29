fc50ecfbca58b98bbcb3f93e0a7e19431ee21fdf
Lidia Reshnivetska <lidiareshnivetska@Lidias-MacBook-Pro.local>
2026-06-29 18:28:12 +0200

    add security group automation + correcting chmod

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -23,6 +23,15 @@ resource "aws_security_group" "app_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description              = "MongoDB from Backend"
+    from_port                = 27017
+    to_port                  = 27017
+    protocol                 = "tcp"
+    source_security_group_id = aws_security_group.backend.id
+}
+
+
   egress {
     from_port   = 0
     to_port     = 0
